e80119ba4f5fc1d4c6337e9a60f403e53eb72653
HamzaDLM <hamzadellam@hotmail.com>
2022-09-13 03:23:45 +0100

    struc

--- src/components/homeIndex.vue
+++ src/components/homeIndex.vue
@@ -1,4 +1,6 @@
 <script>
+import axios from "axios"
+import moment from 'moment'
 
 export default {
   name: 'homeIndex',
@@ -7,58 +9,46 @@ export default {
   },
   data() {
     return {
-      data: [
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        {
-          title: "github_repo_name",
-          description: "This small python app takes in an excel file with data and a corresponding word document template and mass generate files",
-          date: "01/30/2022",
-          language: "Python"
-        },
-        
-      ]
+      items: [],
+      githubUser: "HamzaDLM",
+      avatarURL: "https://avatars.githubusercontent.com/HamzaDLM"
     } 
+  },
+  created() {
+    this.getRepos()
+  },
+  methods: {
+    formatDate(param) {
+      return moment(param).format("MMM Do YY")
+    },
+    pillsColor(language) {
+      switch (language) {
+        case "Python": return "blue"
+        case "Vue": return "lightgreen"
+        case "Javascript": return "yellow"
+        default: return "var(--primary)"
+      }
+    },
+    sorted(param) {
+      param.sort(function (a, b) {
+        return new Date(b.updated_at) - new Date(a.updated_at);
+      })
+    },
+    getAvatar() {
+      this.avatarURL = "https://avatars.githubusercontent.com/" + this.githubUser
+    } ,
+    async getRepos() {
+      return await axios.create({baseURL: "https://api.github.com/users/" + this.githubUser + "/repos"})
+        .get()
+        .then((response) => {
+            this.items = response.data
+            this.items = this.items.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+            this.getAvatar()
+          })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
   }
 }
 
@@ -69,7 +59,7 @@ export default {
     <div class="container top-container d-flex justify-content-center">
       <div class="row mt-5">
         <div class="col-3">
-          <img src="https://avatars.githubusercontent.com/HamzaDLM" 
+          <img :src="avatarURL"  
             class="rounded-circle user-avatar mb-3"
             style="width: 100px;"
             alt="Avatar"
@@ -77,24 +67,40 @@ export default {
         </div>
         <div class="col-9">
           <h1 class="title mt-2">My Github Repos</h1>
-          <p class="text-muted user-name ">@HamzaDLM</p>
+          <!-- <p class="text-muted user-name ">@HamzaDLM</p>
+           -->
+          <div class="d-flex">
+            <input
+                type="search"
+                class="form-control search rounded"
+                placeholder="Type your github username"
+                aria-label="Search"
+                aria-describedby="search-addon"
+                v-model="githubUser"
+            >
+            <a href="#" @click="getRepos()"><i class="fas m-2 fa-search" /></a>
+          </div>
         </div>
       </div>
     </div>
 
     <div class="container mid-container">
-      <div class="row mt-2 scolling">
-        <div v-for="(item, index) in data" :key="index" class="col-3">
-          <div class="card repo-card m-4" style="width: 18rem; height: 14rem;">
+      <div class="row mt-3 scroll">
+        <div v-for="(item, index) in items" :key="index" class="col col-sm-3 col-md-6 col-lg-3">
+          <div class="card repo-card m-2" style="width: 18rem; height: 14rem;">
             <div class="card-body">
-              <p class="card-title">{{ item.title }}</p>
+              <p class="card-title">{{ item.name }}</p>
               <p class="card-text">{{ item.description }}</p>
-              <div class="row">
-                <div class="col-6">
-                  <p class="time"><span class="badge rounded-pill text-bg-secondary">.</span> {{ item.date }}</p>
+              <div class="row" style="position:absolute; bottom:0;">
+                <div class="col">
+                  <p class="time">
+                    <span class="badge rounded-pill text-bg-secondary">
+                      <i class="fa-regular fa-timer"></i>
+                    </span> {{ formatDate(item.updated_at) }}
+                  </p>
                 </div>
-                <div class="col-6">
-                  <span class="badge rounded-pill text-bg-primary">Python</span>
+                <div class="col">
+                  <span class="badge rounded-pill lang text-bg-primary">{{ item.language }}</span>
                 </div>
               </div>
             </div>
@@ -103,6 +109,14 @@ export default {
       </div>
     </div>
 
+    <footer class="text-center text-muted fixed-bottom">
+      <div class="text-center p-3">
+        <p class="m-0"> Copyright free ♥ <span>
+          <a href="https://github.com/HamzaDLM" target="_blank">Developer's Github</a>
+        </span></p>
+      </div>
+    </footer>
+    
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { library, dom } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+library.add(fas, far, fab);
+dom.watch();
 
-createApp(App).mount('#app')
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js"
